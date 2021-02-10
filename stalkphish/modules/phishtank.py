ec4357ee2c8666612410eddb5f6b497ea5254038
KCHARGING <42954503+KCHARGING@users.noreply.github.com>
2021-02-10 15:09:36 -0600

    Initialize KCHARGING-patch-1

---
+++ stalkphish/modules/phishtank.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This file is part of StalkPhish - see https://github.com/t4d/StalkPhish
+
+import re
+import os
+import sys
+import json
+import socket
+import requests
+from os.path import dirname
+from urllib.parse import urlparse
+from tools.utils import TimestampNow
+from tools.utils import UAgent
+
+
+# PhishTank
+def PhishtankOSINT(phishtank_file, ConfPHISHTANK_url, ConfPHISHTANK_keep, SrcDir, PROXY, LOG):
+    # Get phishtank OSINT JSON file
+    proxies = {'http': PROXY, 'https': PROXY}
+    LOG.info("Retrieving Phishtank's JSON file... Could take several minutes...")
+    resp = requests.get(url=ConfPHISHTANK_url, proxies=proxies, allow_redirects=True)
+
+    # download PhishTank JSON file
+    if str(resp.status_code) == "403":
+        LOG.error("PhishTank refused your connection (HTTP 403 code). Maybe Cloudflare asking for a captcha? Or there is an API key problem.")
+        sys.exit(0)
+
+    if str(resp.status_code) != "509":
+        with open(phishtank_file, "wb") as file:
+            file.write(resp.content)
+            LOG.info("Phishtank\'s file retrieved. Proceeding to extraction...")
+    # Error if download limit exceeded
+    else:
+        LOG.error("PhishTank download limit exceeded. Can't download JSON file. Maybe you should use an API key?")
+        sys.exit(0)
+
+
+def PhishtankExtractor(phishtank_file, SearchString, LOG, SQL, TABLEname, PROXY, UAFILE):
+    UAG = UAgent()
+    # Search in Phishtank JSON file
+    file = json.loads(open(phishtank_file).read())
+    for entry in file:
+        # Search
+        if SearchString in entry['url']:
+            # remove URL containing UID-style strings
+            siteURL = re.split("(?:[0-9a-fA-F]:?){32}", entry['url'])[0]
+            dn = dirname(siteURL)
+
+            # Test if entry still exist in DB
+            if SQL.SQLiteVerifyEntry(TABLEname, dn) is 0:
+
+                IPaddress = entry['details'][0]['ip_address']
+                source_url = entry['phish_detail_url']
+                siteDomain = urlparse(entry['url']).netloc
+                now = str(TimestampNow().Timestamp())
+                try:
+                    IPaddress = socket.gethostbyname(siteDomain)
+                # can't resolv
+                except:
+                    IPaddress = ""
+
+                # HTTP connection
+                try:
+                    proxies = {'http': PROXY, 'https': PROXY}
+                    UA = UAG.ChooseUA(UAFILE)
+                    user_agent = {'User-agent': UA}
+                    try:
+                        r = requests.get(siteURL, headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5, 12))
+                        lastHTTPcode = str(r.status_code)
+                    except ValueError:
+                        # No user-agent configured
+                        r = requests.get(siteURL, proxies=proxies, allow_redirects=True, timeout=(5, 12))
+                        lastHTTPcode = str(r.status_code)
+                    except requests.exceptions.Timeout:
+                        lastHTTPcode = "timeout"
+                    except requests.exceptions.ConnectionError:
+                        lastHTTPcode = "aborted"
+                    except:
+                        lastHTTPcode = "---"
+                        err = sys.exc_info()
+                        LOG.error("HTTP error: " + str(err))
+                        pass
+                except:
+                    # Unknown status code
+                    err = sys.exc_info()
+                    LOG.error("Connection error: " + str(err))
+                    pass
+                LOG.info(siteURL)
+                SQL.SQLiteInsertPK(TABLEname, siteURL, siteDomain, IPaddress, source_url, now, lastHTTPcode)
+
+            else:
+                LOG.debug("Entry still known: " + siteURL)
+                pass
+        else:
+            pass
+
+
+# Delete OpenPhish downloaded file, or not
+def DeletePhishtankFile(phishtank_file, LOG):
+    # Delete phishtank_file
+    try:
+        os.remove(phishtank_file)
+        LOG.info("File " + phishtank_file + " deleted.")
+    except:
+        LOG.error("Can't delete " + phishtank_file + " !!!")
+        pass
---
+++ stalkphish/tools/download.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# This file is part of StalkPhish - see https://github.com/t4d/StalkPhish
+
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import sys
+from urllib.parse import urlparse
+from tools.utils import TimestampNow
+from tools.utils import SHA256
+from tools.utils import UAgent
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def PKDownloadOpenDir(siteURL, siteDomain, IPaddress, TABLEname, InvTABLEname, DLDir, SQL, PROXY, LOG, UAFILE):
+    global Ziplst
+    proxies = {'http': PROXY, 'https': PROXY}
+    UAG = UAgent()
+    UA = UAG.ChooseUA(UAFILE)
+    user_agent = {'User-agent': UA}
+    now = str(TimestampNow().Timestamp())
+    SHA = SHA256()
+    Ziplst=[]
+
+    rhtml = requests.get(siteURL, headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5, 12), verify=False)
+    thtml = BeautifulSoup(rhtml.text, 'html.parser')
+    PageTitle = thtml.title.text
+    thtmlatag = thtml.select('a')
+    Ziplst += [siteURL + "/" + tag['href'] for tag in thtmlatag if '.zip' in tag.text]
+    for file in Ziplst:
+        try:
+            r = requests.get(file, headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5, 12), verify=False)
+            zzip = file.replace('/', '_').replace(':', '')
+            if "application/zip" in r.headers['content-type'] or "application/octet-stream" in r.headers['content-type']:
+                savefile = DLDir + zzip
+                # Still collected file
+                if os.path.exists(savefile):
+                    LOG.info("[DL ] Found still collected archive: " + savefile)
+                    return
+                # New file to download
+                else:
+                    LOG.info("[DL ] Found archive in an open dir, downloaded it as: " + savefile)
+                    with open(savefile, "wb") as code:
+                        code.write(r.content)
+                        pass
+                    ZipFileName = str(zzip)
+                    ZipFileHash = SHA.hashFile(savefile)
+                    SQL.SQLiteInvestigUpdatePK(InvTABLEname, siteURL, ZipFileName, ZipFileHash, now, lastHTTPcode)
+                    return
+            else:
+                pass
+        except requests.exceptions.ContentDecodingError:
+            LOG.error("[DL ] content-type error")
+        except:
+            pass
+
+
+# Connexion tests, Phishing kits downloadingd
+def TryPKDownload(siteURL, siteDomain, IPaddress, TABLEname, InvTABLEname, DLDir, SQL, PROXY, LOG, UAFILE):
+    global ziplist
+    global PageTitle
+    proxies = {'http': PROXY, 'https': PROXY}
+    UAG = UAgent()
+    UA = UAG.ChooseUA(UAFILE)
+    user_agent = {'User-agent': UA}
+    now = str(TimestampNow().Timestamp())
+    SHA = SHA256()
+
+    PsiteURL = None
+    ResiteURL = siteURL
+    PsiteURL = urlparse(ResiteURL)
+    if len(PsiteURL.path.split("/")[1:]) >= 2:
+        siteURL = ResiteURL.rsplit('/', 1)[0]
+    else:
+        siteURL = ResiteURL
+
+    # Let's try to find a phishing kit source archive
+    try:
+        r = requests.get(siteURL, headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5, 12), verify=False)
+
+        if (str(r.status_code) != "404"):
+            LOG.info("[" + str(r.status_code) + "] " + r.url)
+            SQL.SQLiteInsertStillTryDownload(TABLEname, siteURL)
+            if SQL.SQLiteInvestigVerifyEntry(InvTABLEname, siteDomain, IPaddress) is 0:
+                SQL.SQLiteInvestigInsert(InvTABLEname, siteURL, siteDomain, IPaddress, now, str(r.status_code))
+            else:
+                pass
+            ziplist = []
+            path = siteURL
+            pathl = '/' .join(path.split("/")[:3])
+            pathlist = path.split("/")[3:]
+
+            # Make list
+            current = 0
+            newpath = ""
+            while current < len(pathlist):
+                if current == 0:
+                    newpath = pathlist[current]
+                else:
+                    newpath = newpath + "/" + pathlist[current]
+                current = current + 1
+                pathD = pathl + "/" + newpath
+                ziplist.append(pathD)
+
+            # Get page title
+            try:
+                if len(ziplist) >= 1:
+                    rhtml = requests.get(siteURL, headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5, 12), verify=False)
+                    thtml = BeautifulSoup(r.text, 'html.parser')
+                    try:
+                        PageTitle = thtml.title.text
+                    except:
+                        PageTitle = None
+                    if PageTitle is not None:
+                        LOG.info(PageTitle)
+                        SQL.SQLiteInvestigUpdateTitle(InvTABLEname, siteURL, PageTitle)
+                    else:
+                        pass
+            except AttributeError:
+                pass
+            except requests.exceptions.ReadTimeout:
+                pass
+            except:
+                err = sys.exc_info()
+                LOG.error("Get PageTitle Error: " + siteURL + str(err))
+
+            try:
+                # Try to find and download phishing kit archive (.zip)
+                if len(ziplist) >= 1:
+                    for zip in ziplist:
+                        if (' = ' or '%' or '?' or '-' or '@') not in os.path.basename(os.path.normpath(zip)):
+                            try:
+                                LOG.info("trying " + zip + ".zip")
+                                rz = requests.get(zip + ".zip", headers=user_agent, proxies=proxies, allow_redirects=True, timeout=(5, 12), verify=False)
+                                if str(rz.status_code) != "404":
+                                    lastHTTPcode = str(rz.status_code)
+                                    zzip = zip.replace('/', '_').replace(':', '')
+                                    try:
+                                        if "application/zip" in rz.headers['content-type'] or "application/octet-stream" in rz.headers['content-type']:
+                                            savefile = DLDir + zzip + '.zip'
+                                            # Still collected file
+                                            if os.path.exists(savefile):
+                                                LOG.info("[DL ] Found still collected archive: " + savefile)
+                                                return
+                                            # New file to download
+                                            else:
+                                                LOG.info("[DL ] Found archive, downloaded it as: " + savefile)
+                                                with open(savefile, "wb") as code:
+                                                    code.write(rz.content)
+                                                    pass
+                                                ZipFileName = str(zzip + '.zip')
+                                                ZipFileHash = SHA.hashFile(savefile)
+                                                SQL.SQLiteInvestigUpdatePK(InvTABLEname, siteURL, ZipFileName, ZipFileHash, now, lastHTTPcode)
+                                                return
+                                        else:
+                                            pass
+                                    except requests.exceptions.ContentDecodingError:
+                                        LOG.error("[DL ] content-type error")
+                                    except:
+                                        pass
+                                # 404
+                                else:
+                                    pass
+                            except requests.exceptions.ReadTimeout:
+                                LOG.debug("Connection Timeout: " + siteURL)
+                            except requests.exceptions.ConnectTimeout:
+                                LOG.debug("Connection Timeout")
+                            except:
+                                err = sys.exc_info()
+                                LOG.error("Error: " + str(err))
+                                pass
+                            # else:
+                            #   pass
+                            try:
+                                if PageTitle is not None:
+                                    if 'Index of' in PageTitle:
+                                        PKDownloadOpenDir(zip, siteDomain, IPaddress, TABLEname, InvTABLEname, DLDir, SQL, PROXY, LOG, UAFILE)
+                                    else:
+                                        pass
+                                else:
+                                    pass
+                            except:
+                                err = sys.exc_info()
+                                LOG.error("Error: " + str(err))
+                                pass
+                        else:
+                            pass
+                    else:
+                        pass
+                # Ziplist empty
+                else:
+                    pass
+            except:
+                err = sys.exc_info()
+                LOG.error("DL Error: " + str(err))
+
+        else:
+            LOG.debug("[" + str(r.status_code) + "] " + r.url)
+            lastHTTPcode = str(r.status_code)
+            SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, lastHTTPcode)
+            SQL.SQLiteInsertStillTryDownload(TABLEname, siteURL)
+
+    except requests.exceptions.ConnectionError:
+        err = sys.exc_info()
+        if '0x05: Connection refused' in err:
+            SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, 'Conn. refused')
+        if '0x04: Host unreachable' in err:
+            SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, 'Unreachable')
+        else:
+            SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, 'Conn. error')
+        SQL.SQLiteInsertStillTryDownload(TABLEname, siteURL)
+        LOG.debug("Connection error: " + siteURL)
+
+    except requests.exceptions.ConnectTimeout:
+        SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, 'Conn. timeout')
+        SQL.SQLiteInsertStillTryDownload(TABLEname, siteURL)
+        LOG.debug("Connection Timeout: " + siteURL)
+
+    except requests.exceptions.ReadTimeout:
+        SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, 'Conn. readtimeout')
+        SQL.SQLiteInsertStillTryDownload(TABLEname, siteURL)
+        LOG.debug("Connection Read Timeout: " + siteURL)
+
+    except requests.exceptions.MissingSchema:
+        SQL.SQLiteInvestigUpdateCode(InvTABLEname, siteURL, now, 'Malformed URL')
+        SQL.SQLiteInsertStillTryDownload(TABLEname, siteURL)
+        LOG.debug("Malformed URL, skipping: " + siteURL + "\n")
+
+    except:
+        err = sys.exc_info()
+        LOG.error("Error: " + str(err))
